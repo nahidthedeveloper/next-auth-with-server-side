abf9e3e1373dcecca1269d26c1918fa9d6d382dd
Nahid <nahidthedeveloper@gmail.com>
2024-11-27 15:26:22 +0600

    delete cookie add in middleware

--- src/middleware.js
+++ src/middleware.js
@@ -7,8 +7,6 @@ export async function middleware(request) {
         req: request,
         secret: process.env.NEXTAUTH_SECRET,
     })
-    const isAdmin = session?.role === 'admin'
-    const currentPath = nextUrl.pathname
 
     const res = await fetch(
         `${process.env.NEXT_PUBLIC_BASE_URL_SERVER}user/user_permissions/`,
@@ -24,14 +22,23 @@ export async function middleware(request) {
     const response = NextResponse.next()
 
     if (user_permissions) {
-        response.cookies.set('user_permissions', JSON.stringify(user_permissions), {
-            path: '/',
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: 'Strict',
-            maxAge: 60 * 60 * 24 * 7,
-        })
+        response.cookies.set(
+            'user_permissions',
+            JSON.stringify(user_permissions),
+            {
+                path: '/',
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: 'Strict',
+                maxAge: 60 * 60 * 24 * 7,
+            }
+        )
+    } else {
+        response.cookies.delete('user_permissions', { path: '/' })
     }
+
+    const isAdmin = session?.role === 'admin'
+    const currentPath = nextUrl.pathname
 
     if (!isAdmin && currentPath.startsWith('/dashboard')) {
         return NextResponse.redirect(new URL('/', request.nextUrl.origin))
