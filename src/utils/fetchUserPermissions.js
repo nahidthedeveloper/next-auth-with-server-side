60d981451f773307c9bbf8308d51a9a7c36760f4
nahid <nahidthedeveloper@gmail.com>
2024-11-28 19:54:21 +0600

    finally fix all problem

--- src/utils/fetchUserPermissions.js
+++ src/utils/fetchUserPermissions.js
@@ -12,7 +12,7 @@ export default async function fetchUserPermissions(token) {
 
         if (token) {
             const res = await fetch(
-                `${process.env.NEXT_PUBLIC_BASE_URL_SERVER}/user/user_permissions/`,
+                `${process.env.NEXT_PUBLIC_BASE_URL_SERVER}user/user_permissions/`,
                 {
                     method: 'GET',
                     headers: {
